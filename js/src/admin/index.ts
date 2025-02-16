ee6d735bb084a6273e792e8b942b84a51ef211eb
Davide Iadeluca <146922689+DavideIadeluca@users.noreply.github.com>
2025-02-16 11:00:42 +0100

    chore: change frontend initializer

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -3,7 +3,7 @@ import SettingsPage from './components/SettingsPage';
 import extendStatusWidget from './extendStatusWidget';
 import extendDashboardPage from './extendDashboardPage';
 
-app.initializers.add('fof/horizon', () => {
+app.initializers.add('fof-horizon', () => {
   app.registry.for('fof-horizon').registerPage(SettingsPage);
   extendStatusWidget();
   extendDashboardPage();
