5bc118bccfbd13f98b3a931a4b349bd5182916af
IanM <ian@morland.me>
2025-11-23 17:16:28 +0000

    chore: use admin js extender

--- js/src/admin/index.ts
+++ js/src/admin/index.ts
@@ -1,10 +1,10 @@
 import app from 'flarum/admin/app';
-import SettingsPage from './components/SettingsPage';
 import extendStatusWidget from './extendStatusWidget';
 import extendDashboardPage from './extendDashboardPage';
 
+export { default as extend } from './extend';
+
 app.initializers.add('fof-horizon', () => {
-  app.registry.for('fof-horizon').registerPage(SettingsPage);
   extendStatusWidget();
   extendDashboardPage();
 });
