3f9e528172ca1a14d9d4edb5c9b6d644051aea8f
IanM <16573496+imorland@users.noreply.github.com>
2025-11-25 21:17:29 +0000

    chore: use StatusWidgetItem (#16)

--- js/src/admin/extendStatusWidget.tsx
+++ js/src/admin/extendStatusWidget.tsx
@@ -1,12 +1,13 @@
 import app from 'flarum/admin/app';
 import { extend } from 'flarum/common/extend';
 import StatusWidget from 'flarum/admin/components/StatusWidget';
+import StatusWidgetItem from 'flarum/admin/components/StatusWidgetItem';
 
 export default function extendStatusWidget() {
   extend(StatusWidget.prototype, 'items', function (items) {
     const cacheStore = app.data.cacheStore;
     const cacheVersion = app.data.cacheVersion;
 
-    items.add('version-cache', [<strong>Cache store</strong>, <br />, `${cacheStore} ${cacheVersion}`], 75);
+    items.add('version-cache', <StatusWidgetItem icon="fas fa-layer-group" label="Cache store" value={`${cacheStore} ${cacheVersion}`} />, 75);
   });
 }
